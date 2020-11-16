2291b3de4b08c8f92b07c63e804b09a7b17fe537
Akshat Agarwal <humancalico@disroot.org>
2020-11-16 17:35:12 +0530

    use color-eyre as an error handling crate

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use dbus::{
 use std::time::Duration;
 use notify_rust::Notification;
 use std::collections::HashMap;
+use color_eyre::eyre::Result;
 
-fn get_media_player(c: &Connection) -> Result<String, Box<dyn std::error::Error>> {
+fn get_media_player(c: &Connection) -> Result<String> {
     let p = c.with_proxy("org.freedesktop.DBus", "/", Duration::from_millis(5000));
 
     let (names,): (Vec<String>,) = p.method_call("org.freedesktop.DBus", "ListNames", ())?;
@@ -20,7 +21,7 @@ fn get_media_player(c: &Connection) -> Result<String, Box<dyn std::error::Error>
     Ok(player_name)
 }
 
-fn get_current_track_info(c: &Connection, player_name: String) -> Result<(), Box<dyn std::error::Error>> {
+fn get_current_track_info(c: &Connection, player_name: String) -> Result<()> {
     let p = c.with_proxy(player_name, "/org/mpris/MediaPlayer2", Duration::from_millis(5000));
     let metadata: HashMap<String, arg::Variant<Box<dyn arg::RefArg>>> = p.get("org.mpris.MediaPlayer2.Player", "Metadata")?;
 
@@ -35,7 +36,8 @@ fn get_current_track_info(c: &Connection, player_name: String) -> Result<(), Box
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
+    color_eyre::install()?;
     let c = Connection::new_session()?;
 
     let player_name = get_media_player(&c).unwrap();
